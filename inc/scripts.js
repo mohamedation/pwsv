616f58d56641c8e524fa7877279ee8bc7f4be456
mohamedation <mohamedation@gmail.com>
2023-03-15 08:01:50 +0200

    misc. fixes

--- inc/scripts.js
+++ inc/scripts.js
@@ -2,8 +2,11 @@ function changeMessage() {
     var input = document.getElementById("password");
     var message = document.getElementById("message");
     var audio = new Audio('inc/sound/ah-ah-ah.mp3');
+    var form = document.getElementById("form-group");
 
     input.value = "!";
-    message.style.visibility = "visible"
-audio.play();
+    input.blur();
+    message.style.opacity = "1";
+    form.classList.add("wiggle");
+    audio.play();
 }
