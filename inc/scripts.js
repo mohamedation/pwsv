e00bf863c6bcc2438aa914ed35f5aee23a3467b5
mohamedation <mohamedation@gmail.com>
2023-03-15 02:18:41 +0200

    initial deployment

--- inc/scripts.js
+++ inc/scripts.js
@@ -1,7 +1,7 @@
 function changeMessage() {
     var input = document.getElementById("password");
     var message = document.getElementById("message");
-    var audio = new Audio('/inc/sound/ah-ah-ah.mp3');
+    var audio = new Audio('inc/sound/ah-ah-ah.mp3');
 
     input.value = "!";
     message.style.visibility = "visible"
